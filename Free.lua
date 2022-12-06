68293f80de5c2de7bb1b9cb111ca0c8024a3b98f
DarkBoisHUB <99702280+Bruh123aaa@users.noreply.github.com>
2022-12-06 17:26:15 +0700

    Update Free.lua

--- Free.lua
+++ Free.lua
@@ -4,9 +4,20 @@ local win = DiscordLib:Window("Nexlus Hub")
 
 local serv = win:Server("สคิปต่างๆ", "")
 
-local btns = serv:Channel("สคิป บล็อกฟุต")
+local bf = serv:Channel("สคิป บล็อกฟุต")
+local kl = serv:Channel("สคิป คิงพีช")
+local asn = serv:Channel("สคิป อาเซนอล")
 
-btns:Button("Neva hub บล็อกฟุต", function()
+bf:Button("Neva hub บล็อกฟุต", function()
 --Blox fruit-Neva-Hub
 loadstring(game:HttpGet('https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2'))()
+end)
+
+kl:Button("One X Hub คิงพีช", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/King-Legacy/main/README.md"))()
+end)
+
+asn:Button("อาเซนอล จำชื่อ Hubไม่ได้555!!", function()
+--Arsenal
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YellowGreg/UltimateCromaSin-Arsenal-Gui/main/AdvanceTechV2.lua"))()
 end)
